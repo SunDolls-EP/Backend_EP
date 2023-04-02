3284a5c852803b9d38bf673a86302c66f790e1bf
unavailableusername09 <legeno09133@gmail.com>
2023-04-02 21:07:53 +0900

    initial commit

---
+++ src/main/java/com/sundolls/epbackend/EpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sundolls.epbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sundolls/epbackend/EpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sundolls.epbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
